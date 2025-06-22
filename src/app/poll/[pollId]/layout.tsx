df5c9b0b27b42763e13c0046ff9200cd70c88fd4
gabrieltemtsen <gabrieltemtsen@gmail.com>
2025-06-22 02:50:41 +0100

    fix up

--- src/app/poll/[pollId]/layout.tsx
+++ src/app/poll/[pollId]/layout.tsx
@@ -1,16 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
 import { generateMetadataForPoll } from '~/app/utils/metada';
 
 
 
-type Props = {
-  children: React.ReactNode;
-  params: { pollId: any };
-};
+
 
 export const generateMetadata = generateMetadataForPoll;
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children }: any) {
   return children;
 }
